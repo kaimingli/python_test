911039d98da1ca58dc3cf29942e0ae2ed946e57a
KAIMINGLI <KAIMINGLI109@163.com>
2017-05-24 23:08:22 +0800

    add listtuple_test

---
+++ listtuple_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+L = [
+	['Apple','Google','Microsoft'],
+	['Java','Python','Ruby','PHP'],
+	['Adam','Bart','Lisa']
+]
+
+print(L[0][1])
+print(L[1][1])
+print(L[2][2])
+input()
