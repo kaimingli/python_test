09e46bf5c143522fc74e408f740702d454f94144
KAIMINGLI <KAIMINGLI109@163.com>
2017-06-01 14:16:14 +0800

    change if_test to chinese

--- if_test.py
+++ if_test.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
-print('1')
+print('输入体重')
 s1 = input()
 weight = int(s1)
-print('2')
+print('输入身高')
 s2 = input()
 height = int(s2)
 
 bim = weight/(height**2)
 if bim<18.5:
-	print('3')
+	print('过轻')
 else:
 	if bim<25:
-		print('4')
+		print('正常')
 	else:
 		if bim<28:
-			print('5')
+			print('过重')
 		else:
 			if bim<32:
-				print('6')
+				print('肥胖')
 			else:
-				print('7')
+				print('严重肥胖')
 
 input()
